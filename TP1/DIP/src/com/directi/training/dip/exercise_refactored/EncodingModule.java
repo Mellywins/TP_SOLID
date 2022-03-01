a0a45e135c65549110610d556233337f3ab82b1d
Mellywins <zouaghi.wow1919@gmail.com>
2022-03-01 21:30:26 +0100

    refactoring DIP+ISP

--- TP1/DIP/src/com/directi/training/dip/exercise_refactored/EncodingModule.java
+++ TP1/DIP/src/com/directi/training/dip/exercise_refactored/EncodingModule.java
@@ -5,7 +5,31 @@ import java.util.Base64;
 
 public class EncodingModule
 {
-    public void encode(IReader reader, IWriter writer) throws IOException
+    IReader reader= null;
+    IWriter writer = null;
+
+    public EncodingModule(Reader reader, Writer writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Writer getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Writer writer) {
+        this.writer = writer;
+    }
+    public void encode() throws IOException
     {
         String aLine = reader.read();
         String encodedLine = Base64.getEncoder().encodeToString(aLine.getBytes());
--- TP1/DIP/src/com/directi/training/dip/exercise_refactored/EncodingModuleClient.java
+++ TP1/DIP/src/com/directi/training/dip/exercise_refactored/EncodingModuleClient.java
@@ -6,16 +6,18 @@ public class EncodingModuleClient
 {
     public static void main(String[] args) throws IOException
     {
-        EncodingModule encodingModule = new EncodingModule();
 
         IReader reader = new MyFileReader(
             "DIP/src/com/directi/training/dip/exercise_refactored/beforeEncryption.txt");
         IWriter writer = new MyFileWriter(
             "DIP/src/com/directi/training/dip/exercise_refactored/afterEncryption.txt");
-        encodingModule.encode(reader, writer);
+        EncodingModule encodingModule = new EncodingModule(reader,writer);
+        encodingModule.encode();
 
         reader = new MyNetworkReader("http", "myfirstappwith.appspot.com", "/index.html");
         writer = new MyDatabaseWriter();
-        encodingModule.encode(reader, writer);
+        EncodingModule encodingModule2 = new EncodingModule(reader,writer);
+
+        encodingModule.encode();
     }
 }
